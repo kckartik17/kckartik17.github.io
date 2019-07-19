440ef5cf1307910466b254dad4bd9ac41c0f6800
kckartik17 <kckartik17@gmail.com>
2019-07-19 16:41:54 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9515ac6fbcb5df3e90b1.js"
+    "url": "webpack-runtime-a5fb203ae7bc8a2d07a8.js"
   },
   {
     "url": "app-851b92bac6c2992aaddb.js"
@@ -36,10 +36,10 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "617f29c0737a486900220f61d4a42831"
+    "revision": "b629ce5fe98c27c8dbfa6ff2a80ddf70"
   },
   {
-    "url": "0-e9336ad64ca7eb3fdf89.js"
+    "url": "0-2259e6da7bdc5534272b.js"
   },
   {
     "url": "component---src-pages-404-js-fdf8cc740ea44395aa17.js"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "741d39cd5a69622cbc72a1f5a4f8bbd2"
+    "revision": "9b17ae5cb4295dcbdd10cc32afdc6bdb"
   },
   {
     "url": "manifest.webmanifest",
